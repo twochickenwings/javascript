cbf56b4cba848df17de08bead6d82230bc97aaa4
twochickenwings <156501216+twochickenwings@users.noreply.github.com>
2024-02-11 19:32:51 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -6,15 +6,25 @@
 // reset 버튼 누르면 게임이 reset된다
 // 5번의 기회를 다 쓰면 게임이 끝난다. (더이상 추측이 불가하고 버튼이 disable된다.)
 // 유저가 1-100 범위 밖에 있는 숫자 입력하면 알려준다. 기회를 깎지 않는다.
-// 유저가 이미 입력한 숫자를 또 입력하면, 알려준다. 기회를 깎지 않는다.
+// 유저가 이미 입력한 숫자를 또 입력하면 알려준다. 기회를 깎지 않는다.
+//정답을 맞췄을때도 go 버튼이 disabled되서 게임이 실행이 안되게금.
 
 let computerNum = 0; //랜덤번호 지정해 줄 변수 필요.
 let playButton = document.getElementById("play-button");
 let userInput = document.getElementById("user-input") ;
 let resultArea = document.getElementById("result-area");
+let resetButton = document.getElementById("reset-button");
+let chances = 5 
+let gameOver = false
+let chanceArea = document.getElementById("chance-area");
+let history = []
+
 //html의 버튼과 인풋에 id 속성을 넣어서 js로 불러오는 작업 필요.
 //Go 버튼에 클릭 이벤트를 넣어주는 작업. play함수를 매개변수로 설정하고 아래에서 play함수 생성.
+
 playButton.addEventListener('click', play);
+resetButton.addEventListener('click',reset);
+userInput.addEventListener('focus',function(){userInput.value=""})
 function pickRandomNum(){
     computerNum = Math.floor(Math.random()*100)+1;
     console.log('정답',computerNum) 
@@ -23,15 +33,49 @@ function pickRandomNum(){
 //소수점에 100을 곱해서 두자리 숫자가 나오게 한 후에
 //Math.floor를 이용해서 소수점 아래는 버리면 정수가 나오게 됨.
 //math.random이 0~1까지 이므로 최대 0~99까지 밖에 나오지 않으니까 1을 더해주는 작업. 0~100까지 범위 조정 가능.
+
 function play(){
     let userValue = userInput.value; //user input의 value값을 들고 와야지 프로그램이 알아먹음.
-    if(userValue < computerNum){
-        resultArea.textContent = "UP!!!"
-    }else if (computerNum < userValue){
-        resultArea.textContent = "DOWN!!!"
-    }else{
-        resultArea.textContent = "맞추셨습니다!!!"
+    if(userValue < 1 || 100 < userValue){
+        resultArea.textContent = "범위 밖의 숫자입니다!!! 1부터 100까지의 값을 입력하세요."
+        return;
     }
+
+    if(history.includes(userValue)){
+        resultArea.textContent=" 이미 입력한 숫자입니다. 다른 숫자를 입력하세요. "
+        return;
+    }
+
+    chances --;
+    chanceArea.textContent = `남은 기회:${chances}번`; //""정적인값에만 쓰임.``동적인 값에쓰임.
+    console.log("chances",chances);
+    if(userValue < computerNum){
+        resultArea.textContent = "UP!!!";
+    }else if (computerNum < userValue){
+        resultArea.textContent = "DOWN!!!";
+    }else{
+        resultArea.textContent = "맞추셨습니다!!!";
+        gameOver = true;
+    }
+
+    history.push(userValue);
+    console.log(history);
+
+    if(chances <1){
+        gameOver = true
+    }
+    if(gameOver == true){
+        playButton.disabled = true
+    }
+}
+function reset(){
+    //user input창이 깨끗하게 정리되고
+    userInput.value = ""
+    //새로운 번호가 생성되고
+    pickRandomNum();
+
+    resultArea.textContent = "결과 값이 나옵니다!"
+
 }
 pickRandomNum() 
 
